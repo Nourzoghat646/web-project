92aecc0c5f0d3a280422f49b9bc3819c9183221a
Nourzoghat646 <zoghatnm@students.rhu.edu.lb>
2024-11-21 01:37:21 +0300

    services update

--- ser.js
+++ ser.js
@@ -1,16 +1,38 @@
-let images = ["img/calendar-check.png",'img/graphic-design.png' ,'img/meeting.png' ];
-let currentIndex = 0;
+let images1 = ["img/calendar-check.png", 'img/graphic-design.png', 'img/meeting.png'];
+let descriptions1 = [
+    "Description for calendar check image.",
+    "Description for graphic design image.",
+    "Description for meeting image."
+];
 
-function updateImage() {
-    document.getElementById('sliderImage').src = images[currentIndex];
+let images2 = ["img/video-camera.png", 'img/web-design.png', 'img/world-wide-web.png'];
+let descriptions2 = [
+    "Description for meeting image.",
+    "Description for calendar check image.",
+    "Description for graphic design image."
+];
+
+let currentIndex1 = 0;
+let currentIndex2 = 0;
+
+function updateImage1() {
+    document.getElementById('sliderImage1').src = images1[currentIndex1];
+    document.getElementById('imageDescription1').innerText = descriptions1[currentIndex1];
 }
 
-document.getElementById('prevBtn').addEventListener('click', function() {
-    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
-    updateImage();
-});
+function updateImage2() {
+    document.getElementById('sliderImage2').src = images2[currentIndex2];
+    document.getElementById('imageDescription2').innerText = descriptions2[currentIndex2];
+}
 
-document.getElementById('nextBtn').addEventListener('click', function() {
-    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
-    updateImage();
-});
+// Automatic change for the first slider every 6 seconds
+setInterval(function() {
+    currentIndex1 = (currentIndex1 === images1.length - 1) ? 0 : currentIndex1 + 1;
+    updateImage1();
+}, 4000);
+
+// Automatic change for the second slider every 6 seconds
+setInterval(function() {
+    currentIndex2 = (currentIndex2 === images2.length - 1) ? 0 : currentIndex2 + 1;
+    updateImage2();
+}, 4000);
